2af7ff551750ea88c219eccd01de1d40b319dde1
bemyhre <ben.myhre35@gmail.com>
2014-08-16 14:12:36 -0500

    added init state and mobile slider.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
+	bigText = $('.text-slider').slick({
+	slidesToScroll: 1,
+	arrows: false,
+	pauseOnHover: false
+
+	});
 	$('.image-slider').slick({
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 5000,
 		arrows: true,
 		pauseOnHover: false,
-		onAfterChange: afterChange
+		onBeforeChange: afterChange
 	});
 });
 
 
 
 function afterChange(x,y){
-	console.log(y);
-	console.log("eat a dick");
-	$('.text-slider').slick({
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 5000,
-		arrows: false,
-		pauseOnHover: false
-		
-	});
+	bigText.slickGoTo(y);
 }
+
+//not a super fan of this implementation. Should have scoped down big text and separated responsibility a bit more.
+//perhaps onload, instantiate both (all three) of the sliders.  
