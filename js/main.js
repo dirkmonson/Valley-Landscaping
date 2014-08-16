cd5fe9af446adb8320264dfe092f9c74d60e43c7
bemyhre <ben.myhre35@gmail.com>
2014-08-16 13:40:35 -0500

    added text and image slider func. To do: handle mobile slider AND init on large

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@ $(document).ready(function(){
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 5000,
-		arrows: false
+		arrows: true,
+		pauseOnHover: false,
+		onAfterChange: afterChange
 	});
 });
+
+
+
+function afterChange(x,y){
+	console.log(y);
+	console.log("eat a dick");
+	$('.text-slider').slick({
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		pauseOnHover: false
+		
+	});
+}
